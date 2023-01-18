88289fb5736c580f7fb0f6a513abe081bfee5b22
danish <danish@debian.loopbrackets>
2023-01-18 19:08:07 +0500

    audio

--- src/App.vue
+++ src/App.vue
@@ -19,13 +19,11 @@
                 >
               </ion-item>
               <ion-item>
-                <ion-button
-                  expand="block"
-                  size="large"
-                  @click="audio()"
+                <ion-button expand="block" size="large" @click="audio()"
                   >Activate microphone</ion-button
                 >
               </ion-item>
+
               <ion-item>
                 <ion-button expand="block" size="large" @click="takePhoto()"
                   >Take a new photo</ion-button
@@ -51,10 +49,19 @@
                   >Request all permissions</ion-button
                 >
               </ion-item>
+              <ion-item>
+                <ion-button class="record">record</ion-button>
+                <ion-button class="stop">stop</ion-button>
+              </ion-item>
+              <ion-item>
+                <div class="sound-clips">
+                  <article class="clip"></article>
+                </div>
+              </ion-item>
             </ion-list>
           </ion-col>
         </ion-row>
-        
+
         <!-- <ion-row>
       <ion-col size="6" :key="photo" v-for="photo in photos">
         <ion-img :src="photo.webviewPath"></ion-img>
@@ -62,11 +69,11 @@
     </ion-row> -->
       </ion-grid>
     </ion-content>
-    
   </ion-page>
 </template>
 <script>
 import { usePhotoGallery } from "@/composable/usePhotogallery";
+
 import {
   IonPage,
   IonHeader,
@@ -79,6 +86,7 @@ import {
   IonGrid,
   IonRow,
   IonCol,
+  alertController
 } from "@ionic/vue";
 
 export default {
@@ -110,6 +118,77 @@ export default {
       gettingLocation: false,
       errorStr: null,
     },
+    
+    audio() {
+      const record = document.querySelector(".record");
+      const stop = document.querySelector(".stop");
+      const soundClips = document.querySelector(".sound-clips");
+      if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+        navigator.mediaDevices
+          .getUserMedia(
+            // constraints - only audio needed for this app
+            {
+              audio: true,
+            }
+          )
+
+          // Success callback
+          .then((stream) => {
+            console.log("test", stream);
+            const mediaRecorder = new MediaRecorder(stream);
+            record.onclick = () => {
+              mediaRecorder.start();
+              record.style.background = "red";
+              record.style.color = "black";
+            };
+            let chunks = [];
+
+            mediaRecorder.ondataavailable = (e) => {
+              chunks.push(e.data);
+            };
+            stop.onclick = () => {
+              mediaRecorder.stop();
+              record.style.background = "";
+              record.style.color = "";
+            };
+            mediaRecorder.onstop = (e) => {
+              // const clipName = prompt("Enter a name for your sound clip");
+
+              const clipContainer = document.createElement("article");
+              const clipLabel = document.createElement("p");
+              const audio = document.createElement("audio");
+              const deleteButton = document.createElement("button");
+
+              clipContainer.classList.add("clip");
+              audio.setAttribute("controls", "");
+              // deleteButton.innerHTML = "Delete";
+              // clipLabel.innerHTML = clipName;
+
+              clipContainer.appendChild(audio);
+              clipContainer.appendChild(clipLabel);
+              clipContainer.appendChild(deleteButton);
+              soundClips.appendChild(clipContainer);
+
+              const blob = new Blob(chunks, { type: "audio/ogg; codecs=opus" });
+              chunks = [];
+              const audioURL = window.URL.createObjectURL(blob);
+              audio.src = audioURL;
+
+              deleteButton.onclick = (e) => {
+                const evtTgt = e.target;
+                evtTgt.parentNode.parentNode.removeChild(evtTgt.parentNode);
+              };
+            };
+          })
+
+          // Error callback
+          .catch((err) => {
+            console.error(`The following getUserMedia error occurred: ${err}`);
+          });
+      } else {
+        console.log("getUserMedia not supported on your browser!");
+      }
+    },
 
     location() {
       if (!("geolocation" in navigator)) {
@@ -129,8 +208,6 @@ export default {
         }
       );
     },
-
-    
   },
 };
 </script>
