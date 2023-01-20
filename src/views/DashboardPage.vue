b21d1db1c324b974456bae59c274a76e5bcc862d
danish <danish@debian.loopbrackets>
2023-01-20 17:26:51 +0500

    gallery image

--- src/views/DashboardPage.vue
+++ src/views/DashboardPage.vue
@@ -33,7 +33,7 @@
                 >
               </ion-item>
               <ion-item>
-                <ion-button expand="block" size="large" @click="fromGallery()"
+                <ion-button expand="block" size="large" @click="selectPhoto()"
                   >Upload photo from gallery</ion-button
                 >
               </ion-item>
@@ -52,7 +52,7 @@
                 >
               </ion-item>
               <ion-item>
-                <ion-button expand="block" size="large" @click="permissions()"
+                <ion-button expand="block" size="large" @click="permission()"
                   >Request all permissions</ion-button
                 >
               </ion-item>
@@ -61,40 +61,36 @@
         </ion-row>
       </ion-grid>
       <ion-modal ref="modal" trigger="open-modal" @willDismiss="onWillDismiss">
-          <ion-grid>
-            <ion-row>
-              <ion-col size="12">
-                <ion-button
-                  class="ion-text-end"
-                  @click="cancel()"
-                  color="danger"
-                  >close</ion-button
-                >
-                
-              </ion-col>
-            </ion-row>
-          </ion-grid>
-        <ion-content> 
+        <ion-grid>
+          <ion-row>
+            <ion-col size="12">
+              <ion-button class="ion-text-end" @click="cancel()" color="danger"
+                >close</ion-button
+              >
+            </ion-col>
+          </ion-row>
+        </ion-grid>
+        <ion-content>
           <ion-card>
-                  <ion-card-header>
-                    <ion-card-title>Geolocation</ion-card-title>
-                  </ion-card-header>
-                  <ion-card-content>
-                    Your Location is  :
-                    <ion-grid>
-                      <ion-row>
-                        <ion-col>
-                          <p>Longitude</p>
-                          <p>Latitude</p>
-                        </ion-col>
-                        <ion-col>
-                          <p>{{longitude}}</p>
-                          <p>{{latitude}}</p>
-                        </ion-col>
-                      </ion-row>
-                    </ion-grid>
-                  </ion-card-content>
-                </ion-card>
+            <ion-card-header>
+              <ion-card-title>Geolocation</ion-card-title>
+            </ion-card-header>
+            <ion-card-content>
+              Your Location is :
+              <ion-grid>
+                <ion-row>
+                  <ion-col>
+                    <p>Longitude</p>
+                    <p>Latitude</p>
+                  </ion-col>
+                  <ion-col>
+                    <p>{{ longitude }}</p>
+                    <p>{{ latitude }}</p>
+                  </ion-col>
+                </ion-row>
+              </ion-grid>
+            </ion-card-content>
+          </ion-card>
         </ion-content>
       </ion-modal>
     </ion-content>
@@ -102,6 +98,7 @@
 </template>
 <script>
 import { usePhotoGallery } from "@/composable/usePhotogallery";
+import { useGallery } from "@/composable/useGallery";
 import { useRouter } from "vue-router";
 
 import {
@@ -116,21 +113,23 @@ import {
   IonGrid,
   IonRow,
   IonCol,
-  IonModal
+  IonModal,
 } from "@ionic/vue";
 export default {
   name: "DashboardPage",
-data(){
-  return{
-    longitude:null,
-    latitude:null
-  }
-},
+  data() {
+    return {
+      longitude: null,
+      latitude: null,
+    };
+  },
   setup() {
     const router = useRouter();
     const { takePhoto } = usePhotoGallery();
+    const { selectPhoto } = useGallery();
     return {
       takePhoto,
+      selectPhoto,
       router,
     };
   },
@@ -170,14 +169,30 @@ data(){
         (pos) => {
           this.gettingLocation = false;
           this.location = pos;
-          this.longitude=pos['coords'].longitude;
-          this.latitude=pos['coords'].latitude;
+          this.longitude = pos["coords"].longitude;
+          this.latitude = pos["coords"].latitude;
         },
         (err) => {
           this.gettingLocation = false;
           this.errorStr = err.message;
         }
       );
+    },
+    permission() {
+      this.geoLocation();
+      navigator.mediaDevices
+        .getUserMedia({audio:true,video:true})
+        .then((mediaStream) => {
+          // const video = document.querySelector("video");
+          // video.srcObject = mediaStream;
+          // video.onloadedmetadata = () => {
+          //   video.play();
+          // };
+        })
+        .catch((err) => {
+          // always check for errors at the end.
+          console.error(`${err.name}: ${err.message}`);
+        });
     },
   },
 };
--- src/views/ImagePage.vue
+++ src/views/ImagePage.vue
@@ -6,7 +6,23 @@
       </ion-toolbar>
     </ion-header>
     <ion-content>
+      
       <ion-grid>
+        <ion-row>
+          <ion-col size="12" >
+            <h5>From Gallery</h5>
+          </ion-col>
+        </ion-row>
+        <ion-row>
+          <ion-col size="6" :key="photo" v-for="photo in photoss">
+            <ion-img :src="photo.webviewPath"></ion-img>
+          </ion-col>
+        </ion-row>
+        <ion-row>
+          <ion-col size="12" >
+            <h5>From Camera</h5>
+          </ion-col>
+        </ion-row>
         <ion-row>
           <ion-col size="6" :key="photo" v-for="photo in photos">
             <ion-img :src="photo.webviewPath"></ion-img>
@@ -18,6 +34,7 @@
 </template>
 <script>
 import { usePhotoGallery } from "@/composable/usePhotogallery";
+import { useGallery } from "@/composable/useGallery";
 
 import {
   IonPage,
@@ -34,8 +51,10 @@ export default {
   
   setup() {
     const {  photos } = usePhotoGallery();
+    const {  photoss } = useGallery();
     return {
       photos,
+      photoss
     };
   },
   components: {
